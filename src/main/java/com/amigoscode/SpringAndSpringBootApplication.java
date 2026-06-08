d0494d67775bd67e2a00c983b107e48621f2c02d
Hammed Lawal <122512872+Lawaltunde@users.noreply.github.com>
2026-06-08 06:13:38 -0400

    Initialize jackson-start

---
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -0,0 +1,168 @@
+package com.amigoscode;
+
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/v1/person")
+@SpringBootApplication
+public class SpringAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SpringAndSpringBootApplication.class,
+                args
+        );
+    }
+
+    public enum Gender {MALE, FEMALE}
+
+    public enum SortingOder {ASC, DESC}
+
+    public record Person(Integer id,
+                         String name,
+                         Integer age,
+                         Gender gender) {
+
+    }
+
+    private static final AtomicInteger idCounter = new AtomicInteger(0);
+
+    public static List<Person> people = new ArrayList<>();
+
+    static {
+        people.add(
+                new Person(
+                        idCounter.incrementAndGet(),
+                        "John",
+                        20,
+                        Gender.MALE
+                )
+        );
+        people.add(
+                new Person(
+                        idCounter.incrementAndGet(),
+                        "Mariam",
+                        18,
+                        Gender.FEMALE)
+        );
+        people.add(
+                new Person(
+                        idCounter.incrementAndGet(),
+                        "Samba",
+                        30,
+                        Gender.MALE)
+        );
+    }
+
+    @GetMapping
+    public List<Person> getPersons(
+            HttpMethod httpMethod,
+            ServletRequest request,
+            ServletResponse response,
+            @RequestHeader("Content-Type") String contentType,
+            @RequestParam(
+                    value = "sort",
+                    required = false,
+                    defaultValue = "ASC"
+            ) SortingOder sort,
+            @RequestParam(
+                    value = "limit",
+                    required = false,
+                    defaultValue = "10"
+            ) Integer limit) {
+        if (sort == SortingOder.ASC) {
+
+            System.out.println(httpMethod);
+            System.out.println(request.getLocalAddr());
+            System.out.println(response.isCommitted());
+            System.out.println(contentType);
+
+            return people.stream()
+                    .sorted(Comparator.comparing(Person::id))
+                    .collect(Collectors.toList());
+        }
+        return people.stream()
+                .sorted(Comparator.comparing(Person::id).reversed())
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<Person>> getPersonById(
+            @PathVariable("id") Integer id
+    ) {
+        Optional<Person> person = people.stream()
+                .filter(p -> p.id.equals(id))
+                .findFirst();
+        return ResponseEntity.ok().body(person);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePersonById(@PathVariable("id") Integer id) {
+        people.removeIf(person -> person.id.equals(id));
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
+        people.add(
+                new Person(
+                        idCounter.incrementAndGet(),
+                        person.name,
+                        person.age(),
+                        person.gender
+                )
+        );
+    }
+
+    public record PersonUpdateRequest(
+            String name,
+            Integer age
+    ) {
+    }
+
+    @PutMapping("{id}")
+    public void updatePerson(@PathVariable("id") Integer id,
+                             @RequestBody PersonUpdateRequest request) {
+        // find person by id
+        people.stream()
+                .filter(p -> p.id.equals(id))
+                .findFirst()
+                .ifPresent(p -> {
+                    var index = people.indexOf(p);
+
+                    if (request.name != null &&
+                            !request.name.isEmpty() &&
+                            !request.name.equals(p.name)) {
+                        Person person = new Person(
+                                p.id,
+                                request.name,
+                                p.age(),
+                                p.gender()
+
+                        );
+                        people.set(index, person);
+                    }
+                    if (request.age != null
+                            && !request.age.equals(p.age)) {
+                        Person person = new Person(
+                                p.id,
+                                p.name,
+                                request.age,
+                                p.gender()
+
+                        );
+                        people.set(index, person);
+                    }
+                });
+    }
+
+}
